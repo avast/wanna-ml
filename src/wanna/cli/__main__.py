9222785978749ada90e1ba63fd83f7d115275a85
João André Da Silva Cunha <joao.silva1@avast.com>
2022-02-01 12:51:58 +0100

    Docs setup update

--- src/wanna/cli/__main__.py
+++ src/wanna/cli/__main__.py
@@ -1,10 +1,18 @@
 from .plugins.runner import PluginRunner
 
 
-def main() -> None:
-    app = PluginRunner()
+runner = PluginRunner()
+app = runner.app
+
+@app.callback()
+def wanna():
+    """
+    Main entrypoint for wanna cli 
+    """
     app.run()
 
+def main() -> None:
+    wanna()
 
 if __name__ == "__main__":
     main()
