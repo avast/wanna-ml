f8791289503cb9b80c1f89accf4540c3cbcfc139
João André Da Silva Cunha <joao.silva1@avast.com>
2022-05-13 10:37:02 +0200

    Adding component loader that replaces env vars, that should be exported by wanna build

---
+++ tests/components/test_wanna_component_loader.py
@@ -0,0 +1,19 @@
+import os
+import pathlib
+import unittest
+
+from wanna.components.loader import load_wanna_component
+
+
+class TestWannaComponentLoader(unittest.TestCase):
+    def test_wanna_component_loader(self):
+        current_dir = pathlib.Path(__file__).parent.resolve()
+        docker_image = "docker.avast.com/some/uri"
+        os.environ["PREDICTOR_DOCKER_URI"] = docker_image
+        comp = load_wanna_component(current_dir / "test_comp.yaml")
+        self.assertEqual(comp.__dict__["component_spec"].implementation.container.image, docker_image)
+
+        del os.environ["PREDICTOR_DOCKER_URI"]
+        comp = load_wanna_component(current_dir / "test_comp.yaml")
+        self.assertNotEqual(comp.__dict__["component_spec"].implementation.container.image, docker_image)
+        self.assertEqual(comp.__dict__["component_spec"].implementation.container.image, "${PREDICTOR_DOCKER_URI}")
