ce48ac883c44851686603292203fafdb460663ce
Jacek Hebda <jacek.hebda@avast.com>
2022-09-06 14:35:23 +0200

    Added pydantic types

--- src/wanna/core/models/gcp_profile.py
+++ src/wanna/core/models/gcp_profile.py
@@ -10,25 +10,25 @@ class GCPProfileModel(BaseModel, extra=Extra.forbid):
     """
     `wanna_profile` section of the yaml config consists of the following inputs:
 
-    - `profile_name` - name of the WANNA GCP Profile, `default` will be used if not specified otherwise.
+    - `profile_name` - [str] name of the WANNA GCP Profile, `default` will be used if not specified otherwise.
       You could use also for example `dev`, `prod`, or any other string.
-    - `project_id` - GCP project id.
-    - `zone` - (optional) GCP location zone.
-    - `region` - (optional) GCP location region. If the zone is set and the region not, we automatically
+    - `project_id` - [str] GCP project id.
+    - `zone` - [str] (optional) GCP location zone.
+    - `region` - [str] (optional) GCP location region. If the zone is set and the region not, we automatically
       parse the region from the zone (e.g., zone `us-east1-c` automatically sets region `us-east1` if the region
       is not supplied by the user).
-    - `labels` - (optional) GCP resource labels that will be added to all GCP resources you create with this profile.
-      By default, we also add a few labels based on `wanna_project` section.
-    - `bucket` - (optional) GCS Bucket that can later be used in uploading manifests, storing logs, and so on depending
-      on the resource type.
-    - `service_account` - (optional) GCP service account that will be used by the created resources.
+    - `labels` - [Dict[str, str]] (optional) GCP resource labels that will be added to all resources you create
+      with this profile. By default, we also add a few labels based on `wanna_project` section.
+    - `bucket` - [str] (optional) GCS Bucket that can later be used in uploading manifests, storing logs, etc.
+      depending on the resource type.
+    - `service_account` - [str] (optional) GCP service account that will be used by the created resources.
     If not specified, usually the default service account for each resource type is used.
-    - `network` - Google Cloud VPC network name
-    - `subnet` - (optional) Google Cloud VPC subnetwork name
-    - `kms_key` - (optional) Customer managed enryption key given in format
-    projects/{project_id}/locations/{region}/keyRings/{key_ring_id}/cryptoKeys/{key_id}
-    - `docker_repository` - Wanna Docker Repository
-    - `docker_registry` - (optional) Wanna Docker Registry, usually in format {region}-docker.pkg.dev
+    - `network` - [str] Google Cloud VPC network name
+    - `subnet` - [str] (optional) Google Cloud VPC subnetwork name
+    - `kms_key` - [str] (optional) Customer managed enryption key given in format
+      projects/{project_id}/locations/{region}/keyRings/{key_ring_id}/cryptoKeys/{key_id}
+    - `docker_repository` - [str] Wanna Docker Repository
+    - `docker_registry` - [str] (optional) Wanna Docker Registry, usually in format {region}-docker.pkg.dev
     """
 
     profile_name: str
--- src/wanna/core/models/wanna_project.py
+++ src/wanna/core/models/wanna_project.py
@@ -7,13 +7,13 @@ class WannaProjectModel(BaseModel, extra=Extra.forbid):
     """
     `wanna_project` section of the yaml config consists of the following inputs:
 
-    - `name` - the name of the wanna project should be unique, this name will be used in the docker service
+    - `name` - [str] the name of the wanna project should be unique, this name will be used in the docker service
       for naming docker images and in labeling GCP resources. Hence it can be used also for budget monitoring.
-    - `version` - Currently used only in labeling GCP resources, we expect to introduce new API versions
+    - `version` - [str] Currently used only in labeling GCP resources, we expect to introduce new API versions
       and then this parameter will gain more importance.
-    - `authors` - List of email addresses, currently used only in GCP resource labeling but soon also in monitoring.
-    - `billing_id` - (optional) Needed for the cost report
-    - `organization_id` - (optional) Needed for the cost report
+    - `authors` - [List[str]] Email addresses, currently used only in GCP resource labeling but soon also in monitoring.
+    - `billing_id` - [str] (optional) GCP Billing ID, needed for the budget report
+    - `organization_id` - [str] (optional) GCP Organization ID, needed for the budget report
     """
 
     name: str
