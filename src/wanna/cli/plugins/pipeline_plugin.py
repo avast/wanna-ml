a4c0170bd7bb8bb860f0baae55dedfa79ede8ed2
João André Da Silva Cunha <joao.silva1@avast.com>
2022-12-08 00:46:14 +0100

    MLOPS-208 - allow wanna to run pipeline in quick mode

--- src/wanna/cli/plugins/pipeline_plugin.py
+++ src/wanna/cli/plugins/pipeline_plugin.py
@@ -92,13 +92,14 @@ class PipelinePlugin(BasePlugin):
         file: Path = wanna_file_option,
         profile_name: str = profile_name_option,
         instance_name: str = instance_name_option("pipeline", "run"),
+        mode: PushMode = push_mode_option,
     ) -> None:
         """
         Run the pipeline as specified in wanna-ml config. This command puts together build, push and run-manifest steps.
         """
         config = load_config_from_yaml(file, gcp_profile_name=profile_name)
         workdir = pathlib.Path(file).parent
-        pipeline_service = PipelineService(config=config, workdir=workdir, version=version)
+        pipeline_service = PipelineService(config=config, workdir=workdir, version=version, push_mode=mode)
         manifests = pipeline_service.build(instance_name)
         pipeline_service.push(manifests, local=False)
         PipelineService.run([str(p) for p in manifests], extra_params=params, sync=sync)
